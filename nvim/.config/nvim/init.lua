0a69f43cc4925c03ea419469c61b5f46d0229c10
Diwash Rai <diwash.rai1993@gmail.com>
2024-02-04 23:41:36 +0000

    Added custom.snippets file for code snippets

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -531,6 +531,7 @@ cmp.setup.cmdline(':', {
 require "custom.opts"
 require "custom.keymaps"
 require "custom.autocmds"
+require "custom.snippets"
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
