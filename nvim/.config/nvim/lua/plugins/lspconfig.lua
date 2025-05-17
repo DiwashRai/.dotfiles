1be64def05759ec36cc9fa6bbe351f3f796db3b8
Diwash Rai <diwash.rai1993@gmail.com>
2025-05-17 14:07:07 +0100

    externally setup clangd requires manually setting it up

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -135,19 +135,14 @@ return {
 				},
 			}
 
-			-- Ensure the servers and tools above are installed
-			--
-			-- To check the current status of installed tools and/or manually install
-			-- other tools, you can run
-			--    :Mason
-			--
-			-- You can press `g?` for help in this menu.
-			--
-			-- `mason` had to be setup earlier: to configure its options see the
-			-- `dependencies` table for `nvim-lspconfig` above.
-
+			-- Manually setup servers that are installed externally
+			require("lspconfig").clangd.setup(servers.clangd)
+			-- Ensures that servers installed with Mason are setup
 			require("mason-lspconfig").setup({
-				ensure_installed = {},
+				ensure_installed = {
+					"lua_ls",
+					"stylua",
+				},
 				automatic_enable = true,
 				automatic_installation = false,
 				handlers = {
