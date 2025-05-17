784ce6e5c55c8403658271aeb7c75fd679b2b2fa
Diwash Rai <diwash.rai1993@gmail.com>
2025-05-17 13:42:49 +0100

    tweaking clangd settings

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -78,16 +78,15 @@ return {
 				clangd = {
 					cmd = { "clangd" },
 					filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-					root_dir = function(fname)
-						return util.root_pattern(
-							".clangd",
-							".clang-tidy",
-							".clang-format",
-							"compile_commands.json",
-							"compile_flags.txt",
-							"configure.ac" -- AutoTools
-						)(fname) or vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
-					end,
+					root_dir = util.root_pattern(
+						".clangd",
+						".clang-tidy",
+						".clang-format",
+						"compile_commands.json",
+						"compile_flags.txt",
+						"configure.ac",
+						".git"
+					),
 					single_file_support = true,
 					capabilities = {
 						textDocument = {
