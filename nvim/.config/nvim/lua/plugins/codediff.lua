3d0d7bc38f4abe8609954794649418848e9d87ca
Diwash Rai <diwash.rai1993@gmail.com>
2026-02-28 13:57:05 +0000

    trim whitespace codediff

--- nvim/.config/nvim/lua/plugins/codediff.lua
+++ nvim/.config/nvim/lua/plugins/codediff.lua
@@ -5,10 +5,10 @@ return {
 		diff = {
 			layout = "side-by-side",
 			max_computation_time_ms = 5000,
-			ignore_trim_whitespace = false,
+			ignore_trim_whitespace = true,
 			cycle_next_hunk = false,
 			cycle_next_file = false,
 			jump_to_first_change = true,
 		},
-	}
+	},
 }
